e6271323b69283e60955d3e46668d42d91d6e82d
Marc-Olivier Thibault <thim1611@usherbrooke.ca>
2022-02-18 15:06:18 -0500

    Installation fonctionnelle. Mise en place d'une démo

--- Startup.cs
+++ Startup.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.SignalR;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
 using PAUL.Hubs;
 
 namespace PAUL
@@ -26,6 +30,16 @@ namespace PAUL
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // ---------------------------------------------------------------------------------------
+            // React
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+
+            // Make sure a JS engine is registered, or you will get an error!
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName)
+              .AddV8();
+            // ---------------------------------------------------------------------------------------
+
             services.AddControllersWithViews();
             services.Configure<ForwardedHeadersOptions>(options =>
             {
@@ -50,8 +64,34 @@ namespace PAUL
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
+
+            // ---------------------------------------------------------------------------------------
+            // React
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //  .AddScript("~/js/First.jsx")
+                //  .AddScript("~/js/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/js/bundle.server.js");
+            });
+            // ---------------------------------------------------------------------------------------
+
             app.UseStaticFiles();
 
+            // ------------------------------------------------------
+            // SignalR
             // app.UseCors(builder =>
             // {
             //     builder.WithOrigins("http://localhost:5000/hub")
@@ -59,6 +99,7 @@ namespace PAUL
             //         .WithMethods("GET", "POST")
             //         .AllowCredentials();
             // });
+            // ------------------------------------------------------
 
             app.UseRouting();
 
@@ -66,7 +107,7 @@ namespace PAUL
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapHub<ChatHub>("/hub");
+                endpoints.MapHub<ChatHub>("/hub"); // SignalR
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
